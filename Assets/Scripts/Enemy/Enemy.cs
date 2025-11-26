275fe9ae1331ee578f561ff1104522567574d535
AlesxanDer1102 <diego.orrego.t@uni.pe>
2025-11-25 23:37:58 -0500

    feat: implement enemy

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    NavMeshAgent agent;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
