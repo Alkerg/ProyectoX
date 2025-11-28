e4c1da1e89a3348e587f0682f37ccb1eed3906ed
AlesxanDer1102 <diego.orrego.t@uni.pe>
2025-11-27 23:08:23 -0500

    feat: actualizar controlador de ratón para cambiar el cursor al pasar sobre enemigos

--- Assets/Scripts/Managers/MouseController.cs
+++ Assets/Scripts/Managers/MouseController.cs
@@ -2,13 +2,39 @@ using UnityEngine;
 
 public class MouseController : MonoBehaviour
 {
-   [SerializeField] private Texture2D cursorTexture;
+    [SerializeField] private Texture2D cursorTexture;
+    [SerializeField] private Texture2D cursorHoverTexture;
+    [SerializeField] private Vector2 clickPosition = Vector2.zero;
 
-   [SerializeField] private Vector2 clickPosition=Vector2.zero;
+    const string ENEMY_TAG = "Enemy";
 
+    private bool isHoveringEnemy = false;
 
-   void Start()
-   {
-       Cursor.SetCursor(cursorTexture, clickPosition, CursorMode.Auto);
-   }
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, clickPosition, CursorMode.Auto);
+    }
+
+    void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+        if (hit.collider != null && hit.collider.CompareTag(ENEMY_TAG))
+        {
+            if (!isHoveringEnemy)
+            {
+                isHoveringEnemy = true;
+                Cursor.SetCursor(cursorHoverTexture, clickPosition, CursorMode.Auto);
+            }
+        }
+        else
+        {
+            if (isHoveringEnemy)
+            {
+                isHoveringEnemy = false;
+                Cursor.SetCursor(cursorTexture, clickPosition, CursorMode.Auto);
+            }
+        }
+    }
 }
